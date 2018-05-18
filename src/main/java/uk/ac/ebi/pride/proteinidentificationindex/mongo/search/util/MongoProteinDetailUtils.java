d648e22d46ae364857debc8628df8c6b35a9a0f8
Tobias-Ternent <tobias@ebi.ac.uk>
2018-05-18 11:01:04 +0100

    Added break to extraction utility method logic. Added Javadoc. Removed TODOs.

--- src/main/java/uk/ac/ebi/pride/proteinidentificationindex/mongo/search/util/MongoProteinDetailUtils.java
+++ src/main/java/uk/ac/ebi/pride/proteinidentificationindex/mongo/search/util/MongoProteinDetailUtils.java
@@ -4,10 +4,19 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
+/**
+ * Extracts protein detail information.
+ */
 public class MongoProteinDetailUtils {
 
   public static final String NAME = "NAME####";
 
+  /**
+   * Extracts protein detail information for a specified type.
+   * @param description the description to look through
+   * @param type the type to find
+   * @return the information according to the type found in the description
+   */
   @SuppressWarnings("WeakerAccess")
   public static String extractInformationByType(List<String> description, String type) {
     String info = "";
@@ -15,16 +24,19 @@ public class MongoProteinDetailUtils {
       for (String text : description) {
         if (text.startsWith(type)) {
           info = text.split(type)[1];
+          break;
         }
       }
     }
     return info;
   }
 
+  /**
+   * Helper method to find the name in the description
+   * @param description the description to look through
+   * @return the name, if one is present.
+   */
   public static String getNameFromDescription(List<String> description) {
     return extractInformationByType(description, NAME);
   }
-
-  // todo javadoc
-  // todo coverage
 }
