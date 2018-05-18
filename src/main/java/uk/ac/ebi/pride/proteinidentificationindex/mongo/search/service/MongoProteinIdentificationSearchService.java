6bf8d8b71e70a9e2bc8676c2bdf5f7e7df96801a
Tobias-Ternent <tobias@ebi.ac.uk>
2018-05-18 12:02:30 +0100

    Added todo.

--- src/main/java/uk/ac/ebi/pride/proteinidentificationindex/mongo/search/service/MongoProteinIdentificationSearchService.java
+++ src/main/java/uk/ac/ebi/pride/proteinidentificationindex/mongo/search/service/MongoProteinIdentificationSearchService.java
@@ -141,4 +141,6 @@ public class MongoProteinIdentificationSearchService {
   public long countByAssayAccession(String assayAccession) {
     return mongoProteinIdentificationRepository.countByAssayAccession(assayAccession);
   }
+
+  // todo coverage
 }
