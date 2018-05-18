05524aab02a91f37e10db6d3f4c8d88b9ea50f52
Tobias-Ternent <tobias@ebi.ac.uk>
2018-05-18 10:43:50 +0100

    Updated MongoProteinIdentificationIndexService unit tests.

--- src/test/java/uk/ac/ebi/pride/proteinidentificationindex/mongo/search/indexer/ProjectMongoProteinIdentificationIndexerTest.java
+++ src/test/java/uk/ac/ebi/pride/proteinidentificationindex/mongo/search/indexer/ProjectMongoProteinIdentificationIndexerTest.java
@@ -1,10 +1,10 @@
 package uk.ac.ebi.pride.proteinidentificationindex.mongo.search.indexer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.ac.ebi.pride.proteinidentificationindex.mongo.search.config.MongoTestConfiguration;
@@ -14,12 +14,16 @@ import uk.ac.ebi.pride.proteinidentificationindex.mongo.search.service.MongoProt
 import uk.ac.ebi.pride.proteinidentificationindex.mongo.search.service.repository.MongoProteinIdentificationRepository;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {MongoTestConfiguration.class})
+@Slf4j
 public class ProjectMongoProteinIdentificationIndexerTest {
 
   private static final String TEST_SUBMITTED_SEQ =
@@ -28,8 +32,7 @@ public class ProjectMongoProteinIdentificationIndexerTest {
   private static final String TEST_SUBMITTED_AC = "D0NNB3";
   private static final String TEST_PROJ_ACC = "PXT000001";
   private static final String TEST_PROTEIN_ACCESSION = "D0NNB3";
-  private static Logger logger =
-      LoggerFactory.getLogger(ProjectMongoProteinIdentificationIndexerTest.class.getName());
+
   @Resource private MongoProteinIdentificationSearchService mongoProteinIdentificationSearchService;
   @Resource private MongoProteinIdentificationIndexService mongoProteinIdentificationIndexService;
   @Resource private MongoProteinIdentificationRepository mongoProteinIdentificationRepository;
@@ -37,6 +40,8 @@ public class ProjectMongoProteinIdentificationIndexerTest {
   @Before
   public void setUp() {
     mongoProteinIdentificationRepository.deleteAll();
+    mongoProteinIdentificationIndexService.setMongoProteinIdentificationRepository(
+        mongoProteinIdentificationRepository);
   }
 
   @Test
@@ -47,7 +52,11 @@ public class ProjectMongoProteinIdentificationIndexerTest {
 
   @Test
   public void testAddProtein() {
-    addD0NNb3();
+    List<MongoProteinIdentification> proteins = new ArrayList<>();
+    mongoProteinIdentificationIndexService.save(proteins);
+    mongoProteinIdentificationIndexService.save(createTestProtein());
+    proteins.add(createTestProtein());
+    mongoProteinIdentificationIndexService.save(proteins);
     MongoProteinIdentification result = mongoProteinIdentificationSearchService.findById(TEST_ID);
     assertNotNull(result);
     long totalCount =
@@ -55,13 +64,33 @@ public class ProjectMongoProteinIdentificationIndexerTest {
     assertEquals(1, totalCount);
   }
 
-  private void addD0NNb3() {
+  private MongoProteinIdentification createTestProtein() {
     MongoProteinIdentification mongoProteinIdentification = new MongoProteinIdentification();
     mongoProteinIdentification.setId(TEST_ID);
     mongoProteinIdentification.setAccession(TEST_PROTEIN_ACCESSION);
     mongoProteinIdentification.setSubmittedAccession(TEST_SUBMITTED_AC);
     mongoProteinIdentification.setSubmittedSequence(TEST_SUBMITTED_SEQ);
     mongoProteinIdentification.setProjectAccession(TEST_PROJ_ACC);
-    mongoProteinIdentificationIndexService.save(mongoProteinIdentification);
+    return mongoProteinIdentification;
+  }
+
+  @After
+  public void deleteProteins() {
+    MongoProteinIdentification protein = createTestProtein();
+    mongoProteinIdentificationIndexService.delete(protein);
+    mongoProteinIdentificationIndexService.save(protein);
+    List<MongoProteinIdentification> proteins = new ArrayList<>();
+    mongoProteinIdentificationIndexService.delete(proteins);
+    proteins.add(protein);
+    mongoProteinIdentificationIndexService.delete(proteins);
+    mongoProteinIdentificationIndexService.save(protein);
+    mongoProteinIdentificationIndexService.deleteById(protein.getId());
+    mongoProteinIdentificationIndexService.save(protein);
+    mongoProteinIdentificationIndexService.deleteByIds(
+        proteins.stream().map(MongoProteinIdentification::getId).collect(Collectors.toList()));
+    mongoProteinIdentificationIndexService.save(protein);
+    mongoProteinIdentificationIndexService.deleteByProjectAccession(protein.getProjectAccession());
+    mongoProteinIdentificationIndexService.save(protein);
+    mongoProteinIdentificationIndexService.deleteByAssayAccession(protein.getAssayAccession());
   }
 }
